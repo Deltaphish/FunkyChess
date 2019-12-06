b42d1d8159ea5b87391119fb83b8302e5b116390
Joakim <joakimanderlind@gmail.com>
2019-12-06 16:08:34 +0100

    added simple ai

--- Chess.hs
+++ Chess.hs
@@ -102,7 +102,7 @@ moves (Piece Knight _ )  = (-2,1) : (-2,-1) : (-1,2) : (-1,-2) :
                            (2,1) : (2,-1) : (1,2) : (1,-2) : []
 moves (Piece Bishop _ )  = (1,1) : (1,-1) : (-1,1) : (-1,-1) : []
 moves (Piece Queen _)    = moves (Piece Tower White) ++ moves (Piece Bishop White) -- Color of no importance
-moves (Piece King _)     = [(dr',dc') | dr' <- [-1..1], dc' <- [-1..1]]
+moves (Piece King _)     = [(dr',dc') | dr' <- [-1..1], dc' <- [-1..1], (dr',dc') /= (0,0)]
 
 
 onBoard :: Pos -> Bool
@@ -112,19 +112,26 @@ onBoard (r,c) = r >= 0 && r <= 7 && c >= 0 && c <= 7
 -- Generates a list of possible destinations for the piece at startPos
 possibleDest :: Board -> Pos -> [Pos]
 possibleDest board startPos
-   | (rank p) `elem` [Pawn,Knight,King] = step $ moves p
-   | otherwise                          = concat [ walk startPos move | move <- moves p]
+   | (rank p) `elem` [Pawn,Knight,King] = concat [ walk startPos move False | move <- moves p]
+   | otherwise                          = concat [ walk startPos move True  | move <- moves p]
    where
       p = fromJust $ board #!> startPos
+      {-
+      collision :: Board -> (Pos,Pos) -> Bool
+      collision board (_,dest) = case destSqr of
+                                    Just p2 -> color p /= color p2
+                                    Nothing -> False
+         where
+            destSqr = board #!> dest
 
       step :: [Direction] -> [Pos]
-      step = (filter onBoard).map (\(a,b) -> (a+(fst startPos),b+(snd startPos)))
+      step = (filter (collision board)).(filter onBoard).map (\(a,b) -> (a+(fst startPos),b+(snd startPos))) -}
 
-      walk :: Pos -> Direction -> [Pos]
-      walk (r,c) (dr,dc)
+      walk :: Pos -> Direction -> Bool -> [Pos]
+      walk (r,c) (dr,dc) b
          | not $ onBoard newPos = []
          | otherwise = case board #!> newPos of
-                          Nothing -> newPos : walk newPos (dr,dc)
+                          Nothing -> if b then newPos : walk newPos (dr,dc) b else [newPos]
                           Just p' -> if color p' == color p
                                         then []
                                         else [newPos]
@@ -148,9 +155,57 @@ movePiece board start dest
 -- Returns the color of the player who has been checkmated
 checkMate :: Board -> Maybe Color
 checkMate = undefined
+-- AI
 
+pieceValue :: Piece -> Int
+pieceValue (Piece r c) = rankValue r * colorWeight c
+   where 
+      colorWeight :: Color -> Int
+      colorWeight White = -1
+      colorWeight Black = 1
 
+      rankValue :: Rank -> Int
+      rankValue Pawn  = 3
+      rankValue Queen = 50
+      rankValue King  = 900
+      rankValue _     = 25
 
+valueBoard :: Board -> Int
+valueBoard (Board b) = valueRow b 0
+   where 
+      valueRow :: [[Square]] -> Int -> Int
+      valueRow [] _ = 0
+      valueRow (r:rs) n = let rowVal = foldr (+) 0 $ map (\p -> pieceValue p) $ catMaybes r in rowVal + valueRow rs (n+1)
+
+getPiecePositions :: Board -> [(Pos,Piece)]
+getPiecePositions (Board b) = getPieceInRows b 0
+   where
+      getPieceInRows :: [[Square]] -> Int -> [(Pos,Piece)]
+      getPieceInRows [] _     = []
+      getPieceInRows (r:rw) n = [((n,c),fromJust (r !! c) )| c <- findIndices isJust r] ++
+                                getPieceInRows rw (n+1)
+
+minMax :: Board -> Color -> (Pos,Pos)
+minMax board aiColor = getBestMove
+   where
+      aiPieces :: Board -> [(Pos,Piece)]
+      aiPieces board = filter (\(_,p) -> color p == aiColor) $ getPiecePositions board
+      
+      possibleAiMoves :: [(Pos,Piece)] -> [(Pos,Pos)]
+      possibleAiMoves = concat . map (\(cord,_) -> zip (repeat cord) (possibleDest board cord))
+
+      possibleFutureBoards :: [(Pos,Pos)] -> [((Pos,Pos),Board)]
+      possibleFutureBoards = map (\(s,d) -> ((s,d),fromJust $ movePiece board s d))
+
+      scoreBoards :: [((Pos,Pos),Board)] -> [(Int,(Pos,Pos),Board)]
+      scoreBoards = map (\(m,b) -> (valueBoard b,m,b))
+
+      outcomes = scoreBoards.possibleFutureBoards.possibleAiMoves.aiPieces
+
+      getBestMove = move where (_,move,_) = head $ sortBy (\(s1,_,_) (s2,_,_) -> compare s1 s2) $ outcomes board
+
+aiMove :: Board -> Board
+aiMove board = let (start,dest) = minMax board Black in fromJust $ movePiece board start dest
 {-
     -- various test functions, delete later
     vizDest :: Board -> Pos -> Board
