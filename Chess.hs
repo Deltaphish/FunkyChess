cbfd5165cf2a4d12ceac466ddb819dfd68c907fb
Joakim <joakimanderlind@gmail.com>
2019-12-09 18:16:27 +0100

    Misc cleaning

--- Chess.hs
+++ Chess.hs
@@ -4,6 +4,7 @@ import Control.Parallel.Strategies
 
 type Pos = (Int,Int)
 type Direction = (Int,Int)
+type Move = (Pos,Pos)
 
 data Color = White | Black deriving Eq
 
@@ -22,6 +23,9 @@ data Piece = Piece{ rank :: Rank
 type Square = Maybe Piece
 
 newtype Board = Board [[Square]]
+
+-- Recursive Tree Datatype for collecting the moves generated for the ai
+data MoveTree = Node Move [MoveTree] deriving Show
 
 instance Show Piece
    where
@@ -54,16 +58,20 @@ instance Show Board where
          showRow ((Just p):row') = show p ++ "|" ++ showRow row'
          showRow (Nothing:row') = " |" ++ showRow row'
 
--- Get from Board
+-- Helper functions for operating on a board
+-- Get square from Board
 (#!>) :: Board -> Pos -> Square
 (Board b) #!> (r,c) = b !! r !! c
--- Place on Board
+
+-- Place a square on a position on a Board
 (#=>) :: Board -> (Pos,Square) -> Board
 (Board b) #=> ((r,c),sqr) = Board $ startRow ++ (startCol ++ sqr : endCol) : (tail endRow)
     where
         (startRow,endRow) = splitAt r b
         (startCol,_:endCol) = splitAt c (head endRow)
 
+
+-- Functions for creating the board
 
 emptyRow :: [Square]
 emptyRow = replicate 8 Nothing
@@ -84,8 +92,16 @@ homeRow color' = map Just homeRow'
 pawnRow :: Color -> [Square]
 pawnRow color' = map Just $ replicate 8 (Piece Pawn color')
 
+initBoard :: Board
+initBoard = Board $ homeRow Black        :
+            pawnRow Black                :
+            replicate 4 emptyRow         ++
+            pawnRow White                :
+            homeRow White                :
+            []
+
 -- Returns a list of possible direction for the piece
--- This does not account for moves requiring special scenarios
+-- This does not account for moves with requirements (Pawn)
 
 moves :: Piece -> [Direction]
 moves (Piece Pawn White) = [(-1,0)]
@@ -94,37 +110,23 @@ moves (Piece Tower _ )   = [(1,0),(-1,0),(0,1),(0,-1)]
 moves (Piece Knight _ )  = [(-2,1),(-2,-1),(-1,2),(-1,-2),
                            (2,1),(2,-1),(1,2),(1,-2)]
 moves (Piece Bishop _ )  = [(1,1),(1,-1),(-1,1),(-1,-1)]
-moves (Piece Queen _)    = moves (Piece Tower White) ++ moves (Piece Bishop White) -- Color of no importance
+moves (Piece Queen _)    = moves (Piece Tower White) ++ moves (Piece Bishop White)
 moves (Piece King _)     = [(dr',dc') | dr' <- [-1..1], dc' <- [-1..1], (dr',dc') /= (0,0)]
 
 
 onBoard :: Pos -> Bool
 onBoard (r,c) = r >= 0 && r <= 7 && c >= 0 && c <= 7
 
-{-
--- Special version when we want to check if a piece is covering a friendly piece
-possibleDest' :: Board -> Pos -> [Pos]
-possibleDest' board startPos
-   | (rank p) == Pawn                   = [(r+dr,c+dc) | dc <- [-1..1]]
-   | (rank p) `elem` [Knight,King]      = concat [ walk startPos move False | move <- moves p]
-   | otherwise                          = concat [ walk startPos move True  | move <- moves p]
-   whereF
-      p = fromMaybe (error ("possibleDest': no piece at position " ++ show startPos))
-             (board #!> startPos)
-
-      (r,c) = startPos
-      (dr,_) = head $ moves p
-
-      walk :: Pos -> Direction -> Bool -> [Pos]
-      walk (r,c) (dr,dc) b
-         | not $ onBoard newPos = []
-         | otherwise = case board #!> newPos of
-                          Nothing -> if b then newPos : walk newPos (dr,dc) b else [newPos]
-                          Just p' -> [newPos]
-            where newPos = (r+dr,c+dc)
--}
+getPiecePositions :: Board -> Color -> [Pos]
+getPiecePositions (Board b) color' = getPieceInRows b 0
+   where
+      getPieceInRows :: [[Square]] -> Int -> [Pos]
+      getPieceInRows [] _     = []
+      getPieceInRows (r:rw) n = [(n,c) | c <- findIndices isJust r, (color $ fromJust ((Board b) #!> (n,c))) == color'] ++
+                                getPieceInRows rw (n+1)
 
 -- Generates a list of possible destinations for the piece at startPos
+-- set checkCoverage to true for checking if a piece is covering another friendly piece
 possibleDest :: Board -> Bool -> Pos -> [Pos]
 possibleDest board checkCoverage startPos
    | (rank p) == Pawn                   = pawnWalk
@@ -160,24 +162,16 @@ possibleDest board checkCoverage startPos
       
 
       walk :: Pos -> Direction -> Bool -> [Pos]
-      walk (r,c) (dr,dc) b
+      walk (r,c) (dr,dc) multiStep
          | not $ onBoard newPos = []
          | otherwise = case board #!> newPos of
-                          Nothing -> if b then newPos : walk newPos (dr,dc) b else [newPos]
+                          Nothing -> if multiStep then newPos : walk newPos (dr,dc) multiStep else [newPos]
                           Just p' -> if checkCoverage || color p' == color p
                                         then []
                                         else [newPos]
             where newPos = (r+dr,c+dc)
 
 -- API functions
-initBoard :: Board
-initBoard = Board $ homeRow Black        :
-            pawnRow Black                :
-            replicate 4 emptyRow         ++
-            pawnRow White                :
-            homeRow White                :
-            []
-
 
 movePiece :: Board -> Move -> Maybe Board
 movePiece board (start,dest)
@@ -187,8 +181,14 @@ movePiece board (start,dest)
 -- Returns the color of the player who has been checkmated
 checkMate :: Board -> Maybe Color
 checkMate = undefined
--- AI
 
+
+-- AI functions
+
+-- Functions for scoring boards
+
+playerColor = White
+aiColor     = Black
 
 rankValue :: Rank -> Int
 rankValue Pawn   = 1
@@ -201,33 +201,24 @@ rankValue King   = 999
 valueBoard :: Board -> Int
 valueBoard board = pieceValueSum + coverageSum
    where 
-      pieceValueSum = sum $ map (getPieceValue' board) $ getPiecePositions board Black ++ getPiecePositions board White
+      pieceValueSum = sum $ map (getPieceValue' board) $ 
+                        getPiecePositions board aiColor ++
+                        getPiecePositions board playerColor
       coverageSum   = getCoverValue board
 
 -- Make the ai value moves that cover its own pieces
 getCoverValue :: Board -> Int
-getCoverValue board = length $ intersect destinations (getPiecePositions board Black)
-   where destinations = concatMap (possibleDest board True) (getPiecePositions board Black)
+getCoverValue board = length $ intersect destinations (getPiecePositions board aiColor)
+   where destinations = concatMap (possibleDest board True) (getPiecePositions board aiColor)
 
+-- Sets 
 getPieceValue' :: Board -> Pos -> Int
 getPieceValue' board (row,col)  
-   | c == White = (-1) * rankValue r
-   | r /= Pawn  = rankValue r 
-   | otherwise  = rankValue r
+   | c == playerColor = negate $ rankValue r
+   | otherwise        =          rankValue r
       where Just (Piece r c) = board #!> (row,col)
 
-getPiecePositions :: Board -> Color -> [Pos]
-getPiecePositions (Board b) color' = getPieceInRows b 0
-   where
-      getPieceInRows :: [[Square]] -> Int -> [Pos]
-      getPieceInRows [] _     = []
-      getPieceInRows (r:rw) n = [(n,c) | c <- findIndices isJust r, (color $ fromJust ((Board b) #!> (n,c))) == color'] ++
-                                getPieceInRows rw (n+1)
-
 --
-type Move = (Pos,Pos)
-
-data MoveTree = Node Move [MoveTree] deriving Show
 
 toLists :: MoveTree -> [[Move]]
 toLists (Node move []) = [[move]]
@@ -266,7 +257,7 @@ scoreMoves board (m:ms) = case movePiece board m of
 
 
 {-
-   Evaluate the ai's next move by creating a tree of all possible moves and their results.
+   Evaluate the ai's next move by creating a trees of all possible move chains from the board.
    Then pick the tree with the greatest score in the lowest node. To make use of parallellization,
    we convert all tree to lists. 
 -}
@@ -283,10 +274,10 @@ evalMoveTree board move n = createMoveTree newBoard (n-1)
 -- N specifies depth
 createMoveTree :: Board -> Int -> [MoveTree]
 createMoveTree _ 0 = []
-createMoveTree board n | n `mod` 2 == 0 = [Node move (evalMoveTree board move n) | p <- pos Black, move <- (genMoves board p)]
+createMoveTree board n | n `mod` 2 == 0 = [Node move (evalMoveTree board move n) | p <- pos aiColor, move <- (genMoves board p)]
                      | otherwise      = [Node bestPlayerMove (evalMoveTree board bestPlayerMove n)]
                      where pos = getPiecePositions board
-                           playerMoves = concat $ filter (not . null) $ map (genMoves board) (pos White)
+                           playerMoves = concat $ filter (not . null) $ map (genMoves board) (pos playerColor)
                            bestPlayerMove = snd $ minimumBy (\(s1,_) (s2,_) -> compare s1 s2) $ scoreMoves board playerMoves
 --Get all ai piece positions
 
