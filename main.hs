323948c646e3d4448664dc503bb2517290aa86e5
Lukas <26006963+Peptski@users.noreply.github.com>
2019-12-16 10:56:26 +0100

    Cleaned up code

--- main.hs
+++ main.hs
@@ -4,22 +4,21 @@
 import ChessAI
 import Board
 import Types
---import GHC.IO.Encoding
---import System.Win32.Console
+import GHC.IO.Encoding
+import System.Win32.Console
 import Data.Maybe
 import Data.List
 
 numbs = "87654321"
 chars = "ABCDEFGH"
+sides = [White, Black]
 
 main = do 
-    --setLocaleEncoding utf8
-    --setConsoleOutputCP 65001
+    setLocaleEncoding utf8
+    setConsoleOutputCP 65001
     putStrLn "\nWelcome to FunkyChess \n-------------------- \nCommands:\n\nEnter the location of a piece to see all possible destinations, Example:B2 \nEnter the location of a piece and the destination with a space between to make a move, Example:B2 B3 \n\nPress Enter to start"
     getLine
-    let board = initBoard
-    print board
-    gameloop board White
+    startGame White
 
 
 gameloop :: Board -> Color -> IO ()
@@ -31,47 +30,42 @@ gameloop b c = do
     let pos = parseInput i
     if pos /= [] then
         if length pos == 1 then
-            if ownPiece b (pos !! 0) c then
-                putStrLn $ unlines $ "" : (map moveDestStr $ possibleMoves b (pos !! 0)) 
+            if ownPiece b (head pos) c then
+                putStrLn $ unlines $ "" : map moveDestStr (possibleMoves b (head pos)) 
             else 
                 putStrLn "Invalid input!\n"
         else 
-            if correctMove b (pos !! 0) (pos !! 1)&& ownPiece b (pos !! 0) c then
+            if correctMove b (head pos) (pos !! 1)&& ownPiece b (head pos) c then
                 if c == White then do
-                    if (pos !! 1) `elem` getPiecePositions b Black then
-                        if (Piece King Black) == fromJust (b #!> (pos !! 1)) then
-                            finish White
-                        else do
-                            print $ show c
-                            putStr " Took a "
-                            print $ rank $ fromJust (b #!> (pos !! 1))
-                            let newb = fromJust $ movePiece b ((pos !! 0), (pos !! 1))
-                            print newb
-                            gameloop newb Black
-                    else do
-                        let newb = fromJust $ movePiece b ((pos !! 0), (pos !! 1))
-                        print newb
-                        gameloop newb Black
+                    putStr ""
+                    handleInput b White pos
                 else do
-                    if (pos !! 1) `elem` getPiecePositions b White then
-                        if (Piece King White) == fromJust (b #!> (pos !! 1)) then
-                            finish Black
-                        else do
-                            print $ show c
-                            putStr " Took a "
-                            print $ rank $ fromJust (b #!> (pos !! 1))
-                            let newb = fromJust $ movePiece b ((pos !! 0), (pos !! 1))
-                            print newb
-                            gameloop newb White
-                    else do
-                        let newb = fromJust $ movePiece b ((pos !! 0), (pos !! 1))
-                        print newb
-                        gameloop newb White
+                    putStr ""
+                    handleInput b Black pos
             else 
                 putStrLn "Invalid input!\n"
     else 
         putStrLn "Invalid input!\n"
     gameloop b c
+        where
+            handleInput :: Board -> Color -> [Pos] -> IO ()
+            handleInput b c pos =
+                if (pos !! 1) `elem` getPiecePositions b (head (sides \\ [c])) then
+                    if Piece King (head (sides \\ [c])) == fromJust (b #!> (pos !! 1)) then
+                        finish c
+                    else do
+                        print $ show c
+                        putStr " Took a "
+                        print $ rank $ fromJust (b #!> (pos !! 1))
+                        makeMove b c pos
+                else
+                    makeMove b c pos
+                where
+                    makeMove :: Board -> Color -> [Pos] -> IO ()
+                    makeMove b c pos = do
+                        let newb = fromJust $ movePiece b (head pos, pos !! 1)
+                        print newb
+                        gameloop newb (head (sides \\ [c]))
 
 
 ownPiece :: Board -> Pos -> Color -> Bool
@@ -80,22 +74,28 @@ ownPiece b p c = p `elem` getPiecePositions b c
 correctMove :: Board -> Pos -> Pos -> Bool
 correctMove b p d = (p, d) `elem` possibleMoves b p
 
-parseInput :: [Char] -> [Pos]
+parseInput :: String -> [Pos]
 parseInput i
-    | pieceInput i = [((fromJust $ elemIndex (i !! 1) numbs),(fromJust $ elemIndex (i !! 0) chars))]
-    | moveInput i  = [((fromJust $ elemIndex (i !! 1) numbs),(fromJust $ elemIndex (i !! 0) chars)), ((fromJust $ elemIndex (i !! 4) numbs),(fromJust $ elemIndex (i !! 3) chars))]
+    | pieceInput i = [(fromJust $ elemIndex (i !! 1) numbs,fromJust $ elemIndex (head i) chars)]
+    | moveInput i  = [(fromJust $ elemIndex (i !! 1) numbs,fromJust $ elemIndex (head i) chars), (fromJust $ elemIndex (i !! 4) numbs, fromJust $ elemIndex (i !! 3) chars)]
     | otherwise    = []
 
-pieceInput :: [Char] -> Bool
+pieceInput :: String -> Bool
 pieceInput i = 
-    (length i == 2) && (((i !! 0) `elem` chars) && ((i !! 1) `elem` numbs))
+    (length i == 2) && ((head i `elem` chars) && ((i !! 1) `elem` numbs))
 
-moveInput :: [Char] -> Bool
+moveInput :: String -> Bool
 moveInput i = 
-    (length i == 5) && (i !! 0) `elem` chars && (i !! 1) `elem` numbs && (i !! 3) `elem` chars && (i !! 4) `elem` numbs 
+    (length i == 5) && head i `elem` chars && (i !! 1) `elem` numbs && (i !! 3) `elem` chars && (i !! 4) `elem` numbs 
 
-moveDestStr :: Move -> [Char]
-moveDestStr m = [chars !! (snd (snd m))] ++ [numbs !! (fst (snd m))]
+moveDestStr :: Move -> String
+moveDestStr m = (chars !! snd (snd m)) : [numbs !! fst (snd m)]
+
+startGame :: Color -> IO ()
+startGame c = do
+    let board = initBoard
+    print board
+    gameloop board c
 
 finish :: Color -> IO ()
 finish c = do
@@ -103,6 +103,4 @@ finish c = do
     print $ show c
     putStrLn "\nPress Enter to play again..."
     getLine
-    let board = initBoard
-    print board
-    gameloop board White
+    startGame White
